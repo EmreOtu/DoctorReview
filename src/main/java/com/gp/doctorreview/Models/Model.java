d2325cadf7732302a5cf97e1e34813e92ccfb6ed
Almighty Pickle <emreotu.coding17@gmail.com>
2024-03-08 15:46:59 +0000

    Model Created

--- src/main/java/com/gp/doctorreview/Models/Model.java
+++ src/main/java/com/gp/doctorreview/Models/Model.java
@@ -1,4 +1,24 @@
 package com.gp.doctorreview.Models;
 
+import com.gp.doctorreview.Views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+
+    private Model() {
+        this.viewFactory = new ViewFactory();
+    }
+
+    public static synchronized Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
+
 }
