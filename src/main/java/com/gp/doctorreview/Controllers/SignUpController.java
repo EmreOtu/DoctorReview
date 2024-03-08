f202cca4beea82d118c87c6f7a1768bca372bf60
Almighty Pickle <emreotu.coding17@gmail.com>
2024-03-08 00:31:32 +0000

    Sign Up Page Design Updated

--- src/main/java/com/gp/doctorreview/Controllers/SignUpController.java
+++ src/main/java/com/gp/doctorreview/Controllers/SignUpController.java
@@ -1,4 +1,14 @@
 package com.gp.doctorreview.Controllers;
 
+import javafx.scene.control.*;
+
 public class SignUpController {
+    public Label signin_lbl;
+    public TextField name_field;
+    public TextField email_field;
+    public PasswordField password_field;
+    public DatePicker date_field;
+    public Button create_an_account_btn;
+    public Button logout_btn;
+    public Label error_msg;
 }
