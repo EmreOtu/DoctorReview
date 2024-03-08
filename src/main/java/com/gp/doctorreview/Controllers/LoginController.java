dec66d832246d79ef1d5171f0bf3dd7d326238dc
Almighty Pickle <emreotu.coding17@gmail.com>
2024-03-08 00:24:34 +0000

    Login Page Design Updated

--- src/main/java/com/gp/doctorreview/Controllers/LoginController.java
+++ src/main/java/com/gp/doctorreview/Controllers/LoginController.java
@@ -1,4 +1,15 @@
 package com.gp.doctorreview.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public Label create_account_lbl;
+    public TextField email_field;
+    public PasswordField password_field;
+    public Button signin_btn;
+    public Button logout_btn;
+    public Label error_msg;
 }
