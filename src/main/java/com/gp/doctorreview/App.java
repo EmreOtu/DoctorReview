a83c4262dcdc0e884969bc46eab28e12415de08b
Almighty Pickle <emreotu.coding17@gmail.com>
2024-03-08 16:00:33 +0000

    Pages Initialized

--- src/main/java/com/gp/doctorreview/App.java
+++ src/main/java/com/gp/doctorreview/App.java
@@ -1,22 +1,14 @@
 package com.gp.doctorreview;
 
+import com.gp.doctorreview.Models.Model;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Patient/HomePage.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login Screen");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        Model.getInstance().getViewFactory().showLoginPage();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/gp/doctorreview/Controllers/LoginController.java
+++ src/main/java/com/gp/doctorreview/Controllers/LoginController.java
@@ -1,15 +1,41 @@
 package com.gp.doctorreview.Controllers;
 
+import com.gp.doctorreview.Models.Model;
+import javafx.application.Platform;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public Label create_account_lbl;
     public TextField email_field;
     public PasswordField password_field;
     public Button signin_btn;
     public Button logout_btn;
     public Label error_msg;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        signin_btn.setOnAction(actionEvent -> onLogin());
+        logout_btn.setOnAction(actionEvent -> Platform.exit());
+        create_account_lbl.setOnMouseClicked(mouseEvent -> onCreateAccount());
+    }
+
+    private void onLogin() {
+        Stage stage = (Stage) error_msg.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showClientHomePage();
+    }
+
+    private void onCreateAccount() {
+        Stage stage = (Stage) error_msg.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showSignUpPage();
+    }
 }
