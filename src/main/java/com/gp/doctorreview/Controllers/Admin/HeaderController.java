d00b8725b0342883540bd0afdf1de46baf9358b0
Almighty Pickle <emreotu.coding17@gmail.com>
2024-03-10 22:44:00 +0000

    Admin Header Implemented

---
+++ src/main/java/com/gp/doctorreview/Controllers/Admin/HeaderController.java
@@ -0,0 +1,21 @@
+package com.gp.doctorreview.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HeaderController implements Initializable {
+    public Label home_btn;
+    public Label messages_btn;
+    public Label doctors_btn;
+    public Label profile_btn;
+    public Button logout_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
