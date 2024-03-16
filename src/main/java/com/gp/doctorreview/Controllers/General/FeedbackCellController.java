a805d674b13e329a279c4d4e2fee4396b28098be
Almighty Pickle <emreotu.coding17@gmail.com>
2024-03-16 01:48:20 +0000

    FeedbackCellFactory Implemented

--- src/main/java/com/gp/doctorreview/Controllers/General/FeedbackCellController.java
+++ src/main/java/com/gp/doctorreview/Controllers/General/FeedbackCellController.java
@@ -1,5 +1,6 @@
 package com.gp.doctorreview.Controllers.General;
 
+import com.gp.doctorreview.Models.Feedback;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,7 +8,7 @@ import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class FeedbackCellContainer implements Initializable {
+public class FeedbackCellController implements Initializable {
     public Label date_lbl;
     public Label sender_name;
     public Label review_point;
@@ -15,6 +16,12 @@ public class FeedbackCellContainer implements Initializable {
     public Label feedback_title;
     public Label feedback_msg;
     public Button delete_btn;
+
+    private final Feedback feedback;
+
+    public FeedbackCellController(Feedback feedback) {
+        this.feedback = feedback;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
